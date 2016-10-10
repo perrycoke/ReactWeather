5cba53ccb5d3cce36aa88ac2ea8fea210077f08d
Kerry Moran <perrycoke@Kerrys-iMac.local>
2016-10-10 10:32:36 +0100

    Style Nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,15 +1,33 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = (props) => {
-	return (
-		<div>
-			<h2>Nav component</h2>
-			<IndexLink to="/">Get Weather</IndexLink>
-			<IndexLink to="/about">About</IndexLink>
-			<IndexLink to="/examples">Examples</IndexLink>
-		</div>
-	)
-}
+var Nav = React.createClass({
+	onSearch(e) {
+		e.preventDefault();
+		alert('Not yet wired up');
+	},
+  render() {
+    return (
+			<div className="top-bar">
+				<div className="top-bar-left">
+					<ul className="menu">
+						<li className="menu-text">React Weather App</li>
+						<li><IndexLink activeClassName="active" to="/">Get Weather</IndexLink></li>
+						<li><IndexLink activeClassName="active" to="/about">About</IndexLink></li>
+						<li><IndexLink activeClassName="active" to="/examples">Examples</IndexLink></li>
+					</ul>
+				</div>
+				<div className="top-bar-right">
+					<form onSubmit={this.onSearch}>
+						<ul className="menu">
+							<li><input type="search" placeholder="Search weather"/></li>
+							<li><input type="submit" className="button" value="Get Weather"/></li>
+						</ul>
+					</form>
+				</div>
+			</div>
+		);
+  }
+});
 
 module.exports = Nav;
